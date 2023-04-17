4ada9834a6f304c2e6efee24fbe9983a668f6f6e
Arnold Szabo <szaboaarnold@gmail.com>
2023-04-17 12:22:36 +0200

    Add lib dependency on consumer side

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.examplelib.ExampleLib;
 import com.example.examplelib.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        ExampleLib exampleLib = new ExampleLib();
+        exampleLib.setup();
     }
 }
